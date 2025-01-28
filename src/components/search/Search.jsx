0731ac62fd69707e823f5c908a7ddd39a985e94c
webQbe <webcubesolutionslk@gmail.com>
2025-01-28 11:02:49 +0530

    Handled searchText change event

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,26 +2,32 @@ import React, { Component } from 'react'
 import TextField from '@mui/material/TextField';
 import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
+
 class Search extends Component {
     state = {
-        searchText: '',
-        amount: 15,
+        searchText: '', // blank by default, updated by onTextChange()
+        amount: 15, 
         apiUrl: 'https://pixabay.com/api',
         apiKey: '',
         images: []
-    }
+    };
+
+    // Update searchText of the state
+    onTextChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    };
 
   render() {
     return (
       <div>
         <TextField 
-            name="searchText"
-            value={this.state.searchText}
-            onChange={this.onTextChange}
+            name="searchText" // Must match state field name
+            value={this.state.searchText} 
+            onChange={this.onTextChange} // Handle searchText change event
             label="Search For Images"
             fullWidth
         />
-        <br /><br />
+        <br /><br /> 
                 <Select
                     name="amount"
                     value={this.state.amount}
