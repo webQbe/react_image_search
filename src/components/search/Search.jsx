e83644c9dfc74c265ed7116436fdbc8dba453bd5
webQbe <webcubesolutionslk@gmail.com>
2025-01-28 10:45:29 +0530

    Added Select Field to enter number of results

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TextField from '@mui/material/TextField';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 class Search extends Component {
     state = {
@@ -20,6 +21,19 @@ class Search extends Component {
             label="Search For Images"
             fullWidth
         />
+        <br /><br />
+                <Select
+                    name="amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                    label="Amount"
+                >
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={10}>10</MenuItem>
+                    <MenuItem value={15}>15</MenuItem>
+                    <MenuItem value={30}>30</MenuItem>
+                    <MenuItem value={50}>50</MenuItem>
+                </Select>
       </div>
     )
   }
